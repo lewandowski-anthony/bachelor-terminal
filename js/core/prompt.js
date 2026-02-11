2d9f5ebbbe2bfd7f65d2aa579ed723b8c81f83f2
LewandowskiAnthony <lewandowski.anthony.pro@gmail.com>
2026-02-11 17:48:52 +0100

    fix(prompt): update server name in prompt display

--- js/core/prompt.js
+++ js/core/prompt.js
@@ -3,5 +3,5 @@ import { USER_STATE } from "../models/userState.js";
 export function getPrompt() {
   if (USER_STATE.state === 'login') return 'login: ';
   if (USER_STATE.state === 'password') return 'password: ';
-  return `${USER_STATE.user.displayName}@server:~$ `;
+  return `${USER_STATE.user.displayName}@bachelor-server:~$ `;
 }
