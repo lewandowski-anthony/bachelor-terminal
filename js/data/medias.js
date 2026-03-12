cb874f6fb49c0dfaeef22a66a8dc3f8fc5a41e99
LewandowskiAnthony <lewandowski.anthony.pro@gmail.com>
2026-03-12 19:19:38 +0100

    fix(games): update media references in blackJackGame and liarGame

--- js/data/medias.js
+++ js/data/medias.js
@@ -71,14 +71,14 @@ export const mediaList = [
     type: 'video',
     data: 'aHR0cHM6Ly95b3V0dS5iZS9VNWdVVWxQNGtUYw==',
     password: 'Z3JhYm91',
-    unlockDate: '2026-03-20T00:00:00Z',
+    unlockDate: '2026-03-13T00:00:00Z',
   },
   {
     name: '05-project_b-ban.mp4',
     type: 'video',
     data: 'aHR0cHM6Ly95b3V0dS5iZS9oWFdQU2ZOeVduaw==',
     password: 'bWluaW9u',
-    unlockDate: '2026-03-13T00:00:00Z',
+    unlockDate: '2026-03-20T00:00:00Z',
   },
   {
     name: '06-project_b-tla.mp4',
