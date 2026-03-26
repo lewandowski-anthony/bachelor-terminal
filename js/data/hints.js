e94dd87ac7a085fdb7b7c57c0f6c372ffe02d69e
LewandowskiAnthony <lewandowski.anthony.pro@gmail.com>
2026-03-26 17:52:16 +0100

    fix(games): update media references in blackJackGame and liarGame

--- js/data/hints.js
+++ js/data/hints.js
@@ -1,97 +1,97 @@
 export const hintList = [
-  {
-    id: 1,
-    title: 'Indice pour trouver le mot de passe de \'logmaster\'',
-    data: '◇● ⊙△ ◆● ☰▲✚✚● ◆● \'◇⊙☌◎▲✚△●✖\' ●✚△ ●▢ ☍◇▲▣✖ ◆▣▢✚ ◇▲ ☍⊙◎◎▲▢◆● ▲⊙⊍⊙△',
-  },
-  {
-    id: 2,
-    title: 'Indice pour trouver le mot de passe du média \'episode-gotham.mp4\'',
-    data: 'C\'est très logique avec le nom du fichier.',
-  },
-  {
-    id: 3,
-    title: 'Indice pour trouver les noms d\'utilisateurs',
-    data: 'La logique pour les utilisateur est exactement la même que pour Franck Torio. Si vous avez son username, pas de problème pour trouver quasi tout ceux disponibles.',
-  },
-  {
-    id: 4,
-    title: 'Que faut-il trouver sur le serveur ?',
-    data: 'Le principal type de données à dénicher dans le serveur sont les medias. Ces derniers seront protégés par des mots de passe, et il faudra trouver ces mots de passe pour les débloquer. Les logs peuvent aussi être intéressants à consulter, on y trouve parfois des informations sensibles.',
-  },
-  {
-    id: 5,
-    title: 'Comment ouvrir le média TLD 2.0',
-    data: 'Le mot de passe est une date lié au projet TLD, au format DD/MM/YYYY.',
-  },
-  {
-    id: 6,
-    title: 'Comment ouvrir le média du projet projet-secret',
-    data: 'Il faudra jouer au casino afin de trouver le mot de passe pour ce media.',
-  },
-  {
-    id: 7,
-    title: 'Comment ouvrir le média du projet cbqp ?',
-    data: 'Il faudra être un très bon menteur pour trouver le mot de passe de ce media.',
-  },
-  {
-    id: 8,
-    title: 'Ouvrir le project_b-ltlpt.mp4',
-    data: 'Il faut jouer avec un oiseau très musclé',
-  },
-  {
-    id: 9,
-    title: 'Ouvrir le project_b-lc.mp4',
-    data: 'Faut y déménager pour la qualité de vie',
-  },
-  {
-    id: 10,
-    title: 'Ouvrir le project_b-bt.mp4',
-    data: 'Quel personnage de scooby doo est mentionné dans le project_b-lc.mp4',
-  },
-  {
-    id: 11,
-    title: 'Ouvrir le project_b-ban.mp4',
-    data: 'Personnages jaune',
-  },
-  {
-    id: 12,
-    title: 'Ouvrir le project_b-ft.mp4',
-    data: 'Nom de famille de franck',
-  },
-  {
-    id: 13,
-    title: 'Ouvrir le project_b-cta.mp4',
-    data: 'Qui joue le personnage de franck dans les vidéos bloqués ?',
-  },
-  {
-    id: 13,
-    title: 'Ouvrir le project_b-cpc.mp4',
-    data: 'Animal totem de Lucas',
-  },
-  {
-    id: 14,
-    title: 'Ouvrir le project_b-slm.mp4',
-    data: 'Nom de la commande pour donner des baffes',
-  },
-  {
-    id: 15,
-    title: 'Ouvrir le project_b-tll.mp4',
-    data: 'Surnom de laurent quand il est fatigué',
-  },
-  {
-    id: 16,
-    title: 'Ouvrir le project_b-devg.mp4',
-    data: 'Lieu de destination evg Cassandra',
-  },
-  {
-    id: 16,
-    title: 'Ouvrir le project_b-tg.mp4',
-    data: 'Jeu de société de l\'équipe',
-  },
-  {
-    id: 17,
-    title: 'Ouvrir le project_b-le.mp4',
-    data: 'Ouverture connue aux echec, gentillet d\'un pays du sud de l\'Europe',
-  }
+    {
+        id: 1,
+        title: 'Indice pour trouver le mot de passe de \'logmaster\'',
+        data: '◇● ⊙△ ◆● ☰▲✚✚● ◆● \'◇⊙☌◎▲✚△●✖\' ●✚△ ●▢ ☍◇▲▣✖ ◆▣▢✚ ◇▲ ☍⊙◎◎▲▢◆● ▲⊙⊍⊙△',
+    },
+    {
+        id: 2,
+        title: 'Indice pour trouver le mot de passe du média \'episode-gotham.mp4\'',
+        data: 'C\'est très logique avec le nom du fichier.',
+    },
+    {
+        id: 3,
+        title: 'Indice pour trouver les noms d\'utilisateurs',
+        data: 'La logique pour les utilisateur est exactement la même que pour Franck Torio. Si vous avez son username, pas de problème pour trouver quasi tout ceux disponibles.',
+    },
+    {
+        id: 4,
+        title: 'Que faut-il trouver sur le serveur ?',
+        data: 'Le principal type de données à dénicher dans le serveur sont les medias. Ces derniers seront protégés par des mots de passe, et il faudra trouver ces mots de passe pour les débloquer. Les logs peuvent aussi être intéressants à consulter, on y trouve parfois des informations sensibles.',
+    },
+    {
+        id: 5,
+        title: 'Comment ouvrir le média TLD 2.0',
+        data: 'Le mot de passe est une date lié au projet TLD, au format DD/MM/YYYY.',
+    },
+    {
+        id: 6,
+        title: 'Comment ouvrir le média du projet projet-secret',
+        data: 'Il faudra jouer au casino afin de trouver le mot de passe pour ce media.',
+    },
+    {
+        id: 7,
+        title: 'Comment ouvrir le média du projet cbqp ?',
+        data: 'Il faudra être un très bon menteur pour trouver le mot de passe de ce media.',
+    },
+    {
+        id: 8,
+        title: 'Ouvrir le project_b-ltlpt.mp4',
+        data: 'Il faut jouer avec un oiseau très musclé',
+    },
+    {
+        id: 9,
+        title: 'Ouvrir le project_b-lc.mp4',
+        data: 'Faut y déménager pour la qualité de vie',
+    },
+    {
+        id: 10,
+        title: 'Ouvrir le project_b-bt.mp4',
+        data: 'Quel personnage de scooby doo est mentionné dans le project_b-lc.mp4',
+    },
+    {
+        id: 11,
+        title: 'Ouvrir le project_b-ban.mp4',
+        data: 'Personnages jaune',
+    },
+    {
+        id: 12,
+        title: 'Ouvrir le project_b-ft.mp4',
+        data: 'Nom de famille de franck',
+    },
+    {
+        id: 13,
+        title: 'Ouvrir le project_b-cta.mp4',
+        data: 'Qui joue le personnage de franck dans les vidéos bloqués ?',
+    },
+    {
+        id: 13,
+        title: 'Ouvrir le project_b-cpc.mp4',
+        data: 'Animal totem de Lucas',
+    },
+    {
+        id: 14,
+        title: 'Ouvrir le project_b-slm.mp4',
+        data: 'Nom de la commande pour donner des baffes',
+    },
+    {
+        id: 15,
+        title: 'Ouvrir le project_b-tll.mp4',
+        data: 'Surnom de laurent quand il est fatigué',
+    },
+    {
+        id: 16,
+        title: 'Ouvrir le project_b-devg.mp4',
+        data: 'Lieu de destination evg Cassandra',
+    },
+    {
+        id: 17,
+        title: 'Ouvrir le project_b-le.mp4',
+        data: 'Ouverture connue aux echec, gentillet d\'un pays du sud de l\'Europe',
+    },
+    {
+        id: 18,
+        title: 'Ouvrir le project_b-tg.mp4',
+        data: 'Jeu de société de l\'équipe',
+    }
 ];
