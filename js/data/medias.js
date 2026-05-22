579fd239673870f8b07c96b5be11b31d48e43825
LewandowskiAnthony <lewandowski.anthony.pro@gmail.com>
2026-05-22 10:00:44 +0200

    feat(medias): add new video media entries with unlock dates

--- js/data/medias.js
+++ js/data/medias.js
@@ -165,64 +165,63 @@ export const mediaList = [
         password: "YmVudGltZQ=="
     },
     {
-        name: 'lepiretruc.mp4',
+        name: '14-lepiretruc.mp4',
         type: 'video',
         data: 'aHR0cHM6Ly95b3V0dS5iZS8wUDdSWklWbzZvaw==',
         unlockDate: '2026-05-22T00:00:00Z'
     },
     {
-        name: 'attrapez-lestous.mp4',
+        name: '15-attrapez-lestous.mp4',
         type: 'video',
         data: 'aHR0cHM6Ly95b3V0dS5iZS81Zm9JYThxSEZ6bw==',
         unlockDate: '2026-05-29T00:00:00Z'
     },
     {
-        name: 'ledébutdesproblèmes.mp4',
+        name: '16-ledébutdesproblèmes.mp4',
         type: 'video',
         data: 'aHR0cHM6Ly95b3V0dS5iZS9MYmN0bE9Xd1VrTQ==',
         unlockDate: '2026-06-05T00:00:00Z'
     },
     {
-        name: 'miaou.mp4',
+        name: '17-miaou.mp4',
         type: 'video',
         data: 'aHR0cHM6Ly95b3V0dS5iZS9nSXRKaFZIWnJaRQ==',
         unlockDate: '2026-06-12T00:00:00Z'
     },
     {
-        name: 'dorémifasol.mp4',
+        name: '18-dorémifasol.mp4',
         type: 'video',
         data: 'aHR0cHM6Ly95b3V0dS5iZS9fM3E2bk9SX3Z0RQ==',
         unlockDate: '2026-06-19T00:00:00Z'
     },
     {
-        name: 'projetc.mp4',
+        name: '19-projetc.mp4',
         type: 'video',
         data: '',
         unlockDate: '2026-06-26T00:00:00Z'
     },
     {
-        name: 'surprisedujour.mp4',
+        name: '20-surprisedujour.mp4',
         type: 'video',
         data: '',
         unlockDate: '2026-07-03T00:00:00Z'
     },
     {
-        name: 'projetb.mp4',
+        name: '21-projetb.mp4',
         type: 'video',
         data: 'aHR0cHM6Ly95b3V0dS5iZS9RdEJ2QjBxOXBtUQ==',
         unlockDate: '2026-07-10T00:00:00Z'
     },
     {
-        name: 'evjf.mp4',
+        name: '22-evjf.mp4',
         type: 'video',
         data: '',
         unlockDate: '2026-07-17T00:00:00Z'
     },
     {
-        name: 'destination:vesoul.mp4',
+        name: '23-destination:vesoul.mp4',
         type: 'video',
         data: 'aHR0cHM6Ly95b3V0dS5iZS9RWFdidmp2NS16NA==',
         unlockDate: '2026-07-24T00:00:00Z'
     }
-
 ];
